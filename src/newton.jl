b1687dc1fa3beacc99b81db48ad4b1a1044544e3
Bernard Mourrain <bernard.mourrain@inria.fr>
2018-07-20 18:50:22 +0200

    update newton

--- src/newton.jl
+++ src/newton.jl
@@ -13,7 +13,7 @@ function vdm_jac(w, Xi, L)
             for v in 1:n
                 J[i,k]*= Xi[v,k]^((L[i].z)[v])
             end
-            for l in 1:n 
+            for l in 1:n
                 J[i,r+(k-1)*n+l] = w[k];
                 for v in 1:n
                     if v==l
@@ -37,14 +37,14 @@ function vdm_iter!(w, Xi, s0, L)
     f = fill(typeof(Xi[1])(0), length(L))
     mnt = moment(w,Xi)
     for i in 1:length(L)
-        f[i] = mnt(L[i].z)-s0[L[i]] 
+        f[i] = mnt(L[i].z)-s0[L[i]]
     end
     print("|ε|=", norm(f))
     J = vdm_jac(w,Xi,L)
     dwXi = J\f
     delta = norm(dwXi)
     println("  δ=", delta)
-    
+
     r = length(w)
     n = size(Xi,1)
     for i in 1:r
@@ -60,7 +60,18 @@ function vdm_iter!(w, Xi, s0, L)
 end
 
 #----------------------------------------------------------------------
-function vdm_newton(w0, Xi0, s0, L, eps::Float64 = 1.e-5, cmax:: Int64 = 10)
+function vdm_newton(w0, Xi0, s0, L;  args...)
+    eps::Float64 = 1.e-5
+    cmax::Int64  = 10
+    for arg in args
+        if arg[1]==:maxit
+            cmax=arg[2]
+        end
+        if arg[1]==:eps
+            eps=arg[2]
+        end
+    end
+
     w = copy(w0)
     Xi = copy(Xi0)
     c = 0; delta = 1.0
@@ -72,4 +83,3 @@ function vdm_newton(w0, Xi0, s0, L, eps::Float64 = 1.e-5, cmax:: Int64 = 10)
 end
 
 #----------------------------------------------------------------------
-

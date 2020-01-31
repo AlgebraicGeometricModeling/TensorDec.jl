c1d5d8f83fcce0b810f63b067297798c16309c42
KHOUJA Rima <rima.khouja@inria.fr>
2020-01-31 11:15:12 +0100

    add apolarpro

--- src/apolar_prod.jl
+++ src/apolar_prod.jl
@@ -1,4 +1,32 @@
 export apolarpro
+#-----------------------------------------------------------------------
+"""
+```
+apolarpro(P,Q) -> Float64
+```
+The apolar product of two homogeneous polynomials P$=\sum_{|α|=d}{\binom{d}{\alpha}p_{α}x^{α}}$ and
+Q$=\sum_{|α|=d}{\binom{d}{\alpha}q_{α}x^{α}}$, of degree d in n variables is given by
+$⟨P,Q⟩_d=\sum_{|α|=d}{\binom{d}{\alpha}\bar{p_{α}}q^{α}}$.
+
+
+Example
+-------
+```jldoctest
+julia> X=@ring x1 x2
+2-element Array{PolyVar{true},1}:
+ x1
+ x2
+
+julia> P=x1^2+2*im*x1*x2+x2^2
+x1² + (0 + 2im)x1x2 + x2²
+
+julia> Q=2*x1^2+3*x1*x2+6x2^2
+2x1² + 3x1x2 + 6x2²
+
+julia> apolarpro(P,Q)
+8.0 - 3.0im
+```
+"""
 using LinearAlgebra
 using MultivariatePolynomials
 using TensorDec

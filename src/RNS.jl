ccca7ca38a18f0ff3ce903dd94ce2754352fe9ad
KHOUJA Rima <rima.khouja@inria.fr>
2020-03-12 17:28:09 +0100

    add RNS

--- src/RNS.jl
+++ src/RNS.jl
@@ -1,25 +1,7 @@
-export gradeval, hessianeval, hpol, cnewton11, op, RNS_SPED, RNS_R, RNS_C
+export cnewton11, RNS_SPED, RNS_R, RNS_C
 using LinearAlgebra
 using MultivariatePolynomials
-using TensorDec
 using DynamicPolynomials
-
-function gradeval(F,X,a)
-        [DynamicPolynomials.differentiate(F, X[i])(a) for i in 1:length(X)]
-end
-
-function hessianeval(F,X,a)
-    n=length(a)
-    v=fill(0.0+0.0im, (n,n))
-    for i in 1:n
-        p=DynamicPolynomials.differentiate(F, X[i])
-        v[i,:]=transpose([DynamicPolynomials.differentiate(p,X[j])(a) for j in 1:n])
-    end
-    v
-end
-function hpol(W,A,X,d)
-    P = sum(W[i]*(transpose(A[:,i])*X)^d for i in 1:length(W))
-end
 function cnewton11(W::Vector, V::Matrix,P)
     r=size(W,1)
     n=size(V,1)
@@ -121,20 +103,6 @@ end
 
 return W2,V1,Ge
 end
-function op(W::Vector, V::Matrix,P)
-    r=size(W,1)
-    d=maxdegree(P)
-    A=fill(0.0+0.0im,r,r)
-    B=fill(0.0+0.0im,r)
-    for i in 1:r
-        A[i,:]=[conj(W[i])*W[j]*(dot(V[:,i],V[:,j]))^d for j in 1:r]
-    end
-    for i in 1:r
-        B[i]=conj(W[i])*P(conj(V[:,i]))
-    end
-    C=A\B
-end
-
 function RNS_SPED(P,r,N)
     d = maxdegree(P)
     X = variables(P)

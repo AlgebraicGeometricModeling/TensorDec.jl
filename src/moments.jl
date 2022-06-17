070e660db71613b7dace8b6417875c0bf4012e0e
KHOUJA Rima <rima.khouja@inria.fr>
2022-06-17 16:36:23 +0200

    add_moments_method

---
+++ src/moments.jl
@@ -0,0 +1,90 @@
+using Distributions, LinearAlgebra, Plots, GaussianMixtures, DynamicPolynomials, MultivariateSeries
+# To make the sum of the entries of the weight vector in a symmetric tensor decomposition equal to 1.
+function mix_nrmlz(w, Xi, d)
+    s   = sum(abs(x) for x in w)
+    w1  = w/s
+    Xi1 = Xi*s^(1/d)
+    if !iseven(d)
+        for i in 1:length(w1)
+            if w1[i] <0
+                w1[i] = - w1[i]
+                Xi1[:,i] = -Xi1[:,i]
+            end
+        end
+    end
+    return w1, Xi1
+end
+"""
+```
+mix_gauss(sigma, mu, lv, N) ⤍ a dataset S
+```
+To sample a dataset of N observations obeying a mixture of multivariate Gaussian distributions i.e., the dataset can be decomposed to r cluster each of this cluster is obeying a multivariate Gaussian distribution.
+* The cluster proportions is given by the  vector lv.
+* The size of the vector lv is equal to the number of clusters.
+* Sigma is a vector of size r where the ith-entry denoted σ_i in this vector means that the cluster i is of covariance matrix σ_i*Id.
+* The r means of size n (the number of features) of the r clusters are represented in columns in a matrix mu.
+"""
+
+function mix_gauss_var_diff(sigma, mu, lv, N)
+    n=length(mu[1])
+    r=length(sigma)
+    Nrm = [MvNormal(mu[i],Diagonal(ones(n)*sigma[i]))  for i in 1:r]
+    Mix_of_Gaussians = MixtureModel(Nrm[1:r],lv)
+    S = rand(Mix_of_Gaussians, N)
+    return S
+end
+"""
+```
+moment_var_diff(S) → gives the first, second and thirs order moments M1, M2 and M3
+                     computed using the dataset S.
+```
+"""
+function moment_var_diff(S) #method of moments
+    n=size(S,1)
+    N=size(S,2)
+    d=3
+    X = (@polyvar x[1:n])[1]
+    E = sum(S[:,i] for i in 1:N)/N
+    K=sum((S[:,i]-E)*(S[:,i]-E)' for i in 1:N)/N
+    v=svd(K).U[:,end]
+    s=svd(K).S[end]
+    M1=sum(S[:,i]*(v'*(S[:,i]-E))^2 for i in 1:N)/N
+    M2=sum(S[:,i]*S[:,i]' for i in 1:N)/N-s*diagm(ones(n))
+    M=sum(dot(S[:,i],X)^d for i in 1:N)/N
+    M3=M-d*(dot(M1,X))*sum(X[i]^(d-1) for i in 1:length(X))
+    M1, M2, M3
+end
+"""
+```
+optdec(w, Xi, M2) → uses the second order moments M2 to identify the cluster proportions w and the matrix of the means in columns Xi.
+```
+"""
+function optdec(w, Xi, M2)
+    n=size(Xi,1)
+    r=size(Xi,2)
+    foo = [w[i]*vec(Xi[:,i]*Xi[:,i]') for i in 1:r]
+    A=hcat(foo...)
+    B=vec(M2)
+    a=(A'*A)\(A'*B)
+    w1=zeros(r)
+    Xi1=zeros(n,r)
+    for i in 1:r
+        w1[i]=w[i]*a[i]^3
+        Xi1[:,i]=Xi[:,i]/a[i]
+    end
+    return w1, Xi1
+end
+"""
+```
+var_diff(w, Xi, M1) → uses the first order moments M1 to identify the variance in each cluster.
+```
+"""
+function var_diff(w, Xi, M1)
+    n = size(Xi,1)
+    r = length(w)
+    A=zeros(n,r)
+    for i in 1:r
+        A[:,i]=w[i]*Xi[:,i]
+    end
+    a= (A'*A)\(A'*M1)
+end

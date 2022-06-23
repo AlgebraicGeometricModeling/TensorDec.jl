c919d7d37df5a410fdca6c1fb864c4b341716626
KHOUJA Rima <rima.khouja@inria.fr>
2022-06-23 14:42:33 +0200

    update_moments.jl

--- src/moments.jl
+++ src/moments.jl
@@ -14,10 +14,9 @@ function mix_nrmlz(w, Xi, d)
     end
     return w1, Xi1
 end
-
 """
 ```
-moment_var_diff(S) → gives the first, second and thirs order moments M1, M2 and M3
+moment_var_diff(S) → gives the first, second and third order moments M1, M2 and M3
                      computed using the dataset S.
 ```
 """
@@ -36,6 +35,7 @@ function moment_var_diff(S) #method of moments
     M3=M-d*(dot(M1,X))*sum(X[i]^(d-1) for i in 1:length(X))
     M1, M2, M3
 end
+
 """
 ```
 optdec(w, Xi, M2) → uses the second order moments M2 to identify the cluster proportions w and the matrix of the means in columns Xi.
@@ -69,4 +69,25 @@ function var_diff(w, Xi, M1)
         A[:,i]=w[i]*Xi[:,i]
     end
     a= (A'*A)\(A'*M1)
+end
+
+"""
+```
+moment_var_diff(S, r) → given the dataset S and the number of cluster r such that r ≤ number of features, this function estimates using the method of moments the cluster proportion, the means and the covariance for each Gaussian distribution within a spherical Gaussian mixture model that fits with the studied data S.
+```
+"""
+function moment_var_diff(S,r) #method of moments
+    d=3
+    M1, M2, M3 = moment_var_diff(S)
+    w1, Mu1 = decompose(M3,r)
+    w2, Mu2 = rne_n_tr_r(M3, copy(w1), copy(Mu1), Dict{String,Any}("maxIter" => 500,"epsIter" => 1.e-3));
+    w, Mu = optdec(w2, Mu2, M2)
+    w, Mu = mix_nrmlz(w, Mu, d)
+    Sigma = var_diff(w,Mu, M1)
+    for i in 1:r
+        if Sigma[i]<0
+        Sigma[i]=-Sigma[i]
+        end
+    end
+    return w, Mu, Sigma
 end
